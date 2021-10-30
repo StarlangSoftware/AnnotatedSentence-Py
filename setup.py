6071a7a32a08f99739d603a9bebb278157ebe359
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 20:03:29 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-AnnotatedSentence',
-    version='1.0.34',
+    version='1.0.35',
     packages=['AnnotatedSentence'],
     url='https://github.com/olcaytaner/AnnotatedSentence-Py',
     license='',
     author='olcaytaner',
     author_email='olcaytaner@isikun.edu.tr',
     description='Annotated Sentence Processing Library',
-    install_requires=['NlpToolkit-WordNet', 'NlpToolkit-NamedEntityRecognition', 'NlpToolkit-PropBank', 'NlpToolkit-DependencyParser', 'NlpToolkit-FrameNet', 'NlpToolkit-SentiNet']
+    install_requires=['NlpToolkit-WordNet', 'NlpToolkit-NamedEntityRecognition', 'NlpToolkit-PropBank', 'NlpToolkit-DependencyParser', 'NlpToolkit-FrameNet', 'NlpToolkit-SentiNet'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
