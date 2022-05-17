fdf79083b03b25b62e5214a4808dfb3335eb462a
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-17 23:38:36 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-AnnotatedSentence',
-    version='1.0.36',
+    version='1.0.37',
     packages=['AnnotatedSentence'],
     url='https://github.com/olcaytaner/AnnotatedSentence-Py',
     license='',
