858d6b0a4f893e243aba6af1e782c05d53c17dcd
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2025-04-02 15:00:01 +0300

    Due to collisions Language is renamed to LanguageType.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-AnnotatedSentence',
-    version='1.0.39',
+    version='1.0.40',
     packages=['AnnotatedSentence'],
     url='https://github.com/olcaytaner/AnnotatedSentence-Py',
     license='',
